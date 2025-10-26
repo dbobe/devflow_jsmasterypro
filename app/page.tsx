c9a6345cdf73e751a2c5fceac317c26a2f2c3a74
dbobe <bobe.david@live.com>
2025-10-26 12:20:28 -0400

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-black text-white">Welcome to the world of Next.js</h1>
+      <h1 className="text-3xl font-black">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to the world of Next.js</h1>
     </div>
   );
 }
